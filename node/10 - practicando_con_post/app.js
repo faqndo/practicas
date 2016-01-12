4fae4fde013392c10056ec242f41d22bdc3731bd
faqndo <facuespinosa97@gmail.com>
2016-01-12 14:47:44 -0300

    agregando practica con post y get en node

---
+++ node/10 - practicando_con_post/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+var app = express();
+
+app.set("view engine", "jade");
+
+app.get("/",function(req,res){
+  res.render("index",{doctype:"html"});
+});
+
+app.get("/algo", function(req,res){
+  res.render("algo",{doctype:"html"})
+});
+
+app.get("/algo/:nombre", function(req,res){
+  res.render("algo2",{doctype:"html", nombre:req.params.nombre});
+});
+
+app.listen(8080);
